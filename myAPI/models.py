a352ffaafb3eb78e57a28d3196cfe84263b79e39
Mindy Kay V. Zaracena <mindykayzara05@gmail.com>
2022-04-08 10:19:08 +0800

    created a new model called vehicles

--- myAPI/models.py
+++ myAPI/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class vehicles(models.Model):
+    name = models.CharField(max_length=60)
+    desc = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
